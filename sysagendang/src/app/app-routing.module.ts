74a70c5efd7591efbb6043987632d6df6d700521
Gabriel Amaral de Oliveira <gabrielamaral7575@gmail.com>
2023-05-23 10:45:55 -0300

    Primeiro commit e remoção dos antigos arquivos

--- sysagendang/src/app/app-routing.module.ts
+++ sysagendang/src/app/app-routing.module.ts
@@ -1,33 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContatoComponent } from './modules/contato/contato.component';
-import { LigacaoComponent } from './modules/ligacao/ligacao.component';
-import { HomeComponent } from './modules/home/home.component';
-import { AddComponent } from './modules/add/add.component';
 
-const routes: Routes = [
-  {
-    path: "",
-    pathMatch: 'full',
-    redirectTo: "home",
-  },
-  {
-    path: "add",
-    component: AddComponent
-  },
-  {
-    path: "home",
-    component: HomeComponent
-  },
-  {
-    path: "contato",
-    component: ContatoComponent
-  },
-  {
-    path: "ligacao",
-    component: LigacaoComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sysagendang/src/app/app.module.ts
+++ sysagendang/src/app/app.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './modules/home/home.module';
-import { LigacaoModule } from './modules/ligacao/ligacao.module';
-import { HeaderModule } from './shared/layout/header/header.module';
-import { MainModule } from './shared/layout/main/main.module';
-import { ContatoModule } from './modules/contato/contato.module';
-import { AddModule } from './modules/add/add.module';
 
 @NgModule({
   declarations: [
@@ -17,14 +10,7 @@ import { AddModule } from './modules/add/add.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HeaderModule,
-    MainModule,
-    LigacaoModule,
-    ContatoModule,
-    HomeModule,
-    AddModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
